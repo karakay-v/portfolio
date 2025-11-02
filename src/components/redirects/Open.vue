be0320584645768b811e77b9474fc8344343c98b
Vladyslav Karakai <hitechnic00074220@gmail.com>
2025-11-02 21:09:26 +0200

    Update routing (#16)
    * change port
    
    * change routing to createWebHashHistory
    
    * update Open component by adding timeout message

--- src/components/redirects/Open.vue
+++ src/components/redirects/Open.vue
@@ -1,14 +1,30 @@
 <template>
     <div class="loader_wrapper">
-        <Loader :theme="Theme.Light"
-                class="loader" />
+         <Loader    v-if="!showTimeoutError"
+                    :theme="Theme.Light"
+                    class="loader" />
+    
+        <div v-else class="error_message">
+            <h4 class="text">{{ timeoutMessage }}</h4>
+            
+            <Button :label="buttonLabel"
+                    :size="ButtonSize.Medium"
+                    :icon="RetryIcon"
+                    :iconPosition="ButtonIconPosition.Right"
+                    class="try_again_button"
+                    @click="tryAgain" />
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 import Loader from '../UI/Loader.vue';
 import { Theme } from '../../types/theme';
+import Button from '../UI/Button.vue';
+import { ButtonSize } from '../../types/button-types/button-size';
+import RetryIcon from '../../assets/icons/retry.svg';
+import { ButtonIconPosition } from '../../types/button-types/button-icon-position';
 
 function isValidLink(value: string): boolean {
   if (value === '') return true; // allow empty (not required)
@@ -39,10 +55,14 @@ export default defineComponent({
     data() {
         return {
             Theme,
+            ButtonSize,
+            RetryIcon,
+            ButtonIconPosition,
         };
     },
     components: {
         Loader,
+        Button,
     },
     props: {
         path: {
@@ -50,27 +70,72 @@ export default defineComponent({
             required: false,
             validator: (value: string) => isValidLink(value),
         },
-        timeout: {
+        delay: {
             type: Number,
             default: 1000,
             required: false,
         },
+        timeout: {
+            type: Number,
+            default: 5000,
+            required: false,
+        },
+        timeoutMessage: {
+            type: String,
+            default: "Hmm… the magic didn’t work this time.",
+            required: false,
+        },
+        buttonLabel: {
+            type: String,
+            default: "Try again!",
+            required: false,
+        },
     },
     setup(props) {
+        const TRY_AGAIN_DELAY: number = 300;
+
+        const showTimeoutError = ref(false);
+
+        const doRedirect = () => {
+            if (props.path && isValidLink(props.path)) {
+                window.location.href = props.path;
+            } else { 
+                window.location.href = '/';
+            }
+        }
+
+        const tryAgain = () => {
+            showTimeoutError.value = false;
+            
+            setTimeout(() => {
+                doRedirect();
+                
+            }, TRY_AGAIN_DELAY);
+
+            setTimeout(() => {
+                showTimeoutError.value = true;
+            }, props.timeout);        
+        };
+
         onMounted(() => {
             setTimeout(() => {
-                if (props.path && isValidLink(props.path)) {
-                    window.location.href = props.path;
-                } else {
-                    window.location.href = '/';
-                }
+                doRedirect();
+            }, props.delay);
+            
+            setTimeout(() => {
+                showTimeoutError.value = true;
             }, props.timeout);
         });
+
+        return { showTimeoutError, tryAgain };
     },
 });
 </script>
 
 <style lang="scss" scoped>
+@use "/src/assets/styles/fonts";
+@use "/src/assets/styles/colors" as *;
+
 .loader_wrapper {
     overflow: hidden;
     width: 100vw;
@@ -86,6 +151,20 @@ export default defineComponent({
     @media (max-width: 1260px) {
         .loader {
             transform: scale(1.5);
+        }
+    }
+
+    .error_message {
+        .text {
+            @include fonts.sora-font(700);
+            @include fonts.responsive-font(32, 20, 1440);
+            @include fonts.prevent-selecting;
+            text-align: center;
+            color: $primary-black;
+        }
+
+        .try_again_button {
+            margin: 15px auto 0;
         }
     }
 }
