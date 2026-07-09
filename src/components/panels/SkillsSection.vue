52944bc1bf00ca8d081a09a534e1798f8379b42e
Vladyslav Karakai <hitechnic00074220@gmail.com>
2026-07-09 15:02:01 +0000

    level up the tech stack

--- src/components/panels/SkillsSection.vue
+++ src/components/panels/SkillsSection.vue
@@ -11,16 +11,29 @@
 
             <Skill name="Git" :icon="GitIcon" />
             <Skill name="GitHub" :icon="GitHubIcon" />
-            <Skill name="Linux" :icon="LinuxIcon" />
-            <Skill name="Docker" :icon="DockerIcon" />
-            <Skill name="Postgres" :icon="PostgresIcon" />
             <Skill name="Java" :icon="JavaIcon" />
             <Skill name="Spring Boot" :icon="SpringBootIcon" />
+            <Skill name="Spring Framework" :icon="SpringIcon" />
+            <Skill name="Netflix Eureka" :icon="NetflixEurekaIcon" />
+            <Skill name="Apache Kafka" :icon="ApacheKafkaIcon" />
             <Skill name="HTML5 & CSS3" :icon="Html5AndCss3Icon" />
             <Skill name="Sass" :icon="SassIcon" />
             <Skill name="JavaScript" :icon="JavaScriptIcon" />
+            <Skill name="TypeScript" :icon="TypeScriptIcon" />
             <Skill name="Vue" :icon="VueIcon" />
+            <Skill name="React" :icon="ReactIcon" />
+            <Skill name="Postgres" :icon="PostgresIcon" />
+            <Skill name="MariaDB" :icon="MariaDBIcon" />
+            <Skill name="Bash / Zsh" :icon="BashIcon" />
+            <Skill name="Docker" :icon="DockerIcon" />
+            <Skill name="Linux" :icon="LinuxIcon" />
+            <Skill name="Ubuntu" :icon="UbuntuIcon" />
+            <Skill name="Proxmox" :icon="ProxmoxIcon" />
             <Skill name="AWS EC2" :icon="AwsIcon" />
+            <Skill name="MikroTik" :icon="MikroTikIcon" />
+            <Skill name="Cisco" :icon="CiscoIcon" />
+            <Skill name="Junos" :icon="JunosIcon" />
+            <Skill name="VyOS" :icon="VyosIcon" />
 
         </div>
     </div>
@@ -31,16 +44,29 @@ import { defineComponent } from 'vue';
 import Skill from '../UI/Skill.vue';
 import GitIcon from '../../assets/icons/icon-git.png';
 import GitHubIcon from '../../assets/icons/github.png';
-import LinuxIcon from '../../assets/icons/tux.png';
-import DockerIcon from '../../assets/icons/docker.png';
-import PostgresIcon from '../../assets/icons/postgres.png';
 import JavaIcon from '../../assets/icons/java.png';
 import SpringBootIcon from '../../assets/icons/spring-boot.png';
+import SpringIcon from '../../assets/icons/spring.png';
+import NetflixEurekaIcon from '../../assets/icons/netflix-eureka.png';
+import ApacheKafkaIcon from '../../assets/icons/apache-kafka.png';
 import Html5AndCss3Icon from '../../assets/icons/html5-and-css3.png';
 import SassIcon from '../../assets/icons/icon-sass.png';
 import JavaScriptIcon from '../../assets/icons/icon-javascript.png';
+import TypeScriptIcon from '../../assets/icons/icon-typescript.png';
 import VueIcon from '../../assets/icons/vue.png';
+import ReactIcon from '../../assets/icons/react.png';
+import PostgresIcon from '../../assets/icons/postgres.png';
+import MariaDBIcon from '../../assets/icons/mariadb.png';
+import BashIcon from '../../assets/icons/bash.png';
+import DockerIcon from '../../assets/icons/docker.png';
+import LinuxIcon from '../../assets/icons/tux.png';
+import UbuntuIcon from '../../assets/icons/ubuntu.png';
+import ProxmoxIcon from '../../assets/icons/proxmox.png';
 import AwsIcon from '../../assets/icons/aws.png';
+import MikroTikIcon from '../../assets/icons/mikrotik.png';
+import CiscoIcon from '../../assets/icons/cisco.png';
+import JunosIcon from '../../assets/icons/junos.png';
+import VyosIcon from '../../assets/icons/vyos.png';
 
 export default defineComponent({
     name: "SkillsSection",
@@ -48,16 +74,29 @@ export default defineComponent({
         return({
             GitIcon,
             GitHubIcon,
-            LinuxIcon,
-            DockerIcon,
-            PostgresIcon,
             JavaIcon,
             SpringBootIcon,
+            SpringIcon,
+            NetflixEurekaIcon,
+            ApacheKafkaIcon,
             Html5AndCss3Icon,
             SassIcon,
             JavaScriptIcon,
+            TypeScriptIcon,
             VueIcon,
+            ReactIcon,
+            PostgresIcon,
+            MariaDBIcon,
+            BashIcon,
+            DockerIcon,
+            LinuxIcon,
+            UbuntuIcon,
+            ProxmoxIcon,
             AwsIcon,
+            MikroTikIcon,
+            CiscoIcon,
+            JunosIcon,
+            VyosIcon
         });
     },
     components: {
