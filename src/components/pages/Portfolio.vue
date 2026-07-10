c65e893999da07df054f17e94fbcb49d31e2e071
Vladyslav Karakai <hitechnic00074220@gmail.com>
2026-07-10 03:43:07 +0000

    fix: comment the MyTestimonial component

--- src/components/pages/Portfolio.vue
+++ src/components/pages/Portfolio.vue
@@ -17,7 +17,7 @@ import SkillsSection from '../panels/SkillsSection.vue';
 import MyExpirienceSection from '../panels/MyExpirienceSection.vue';
 import AboutMeSection from '../panels/AboutMeSection.vue';
 import MyProjects from '../panels/MyProjects.vue';
-import MyTestimonial from '../panels/MyTestimonial.vue';
+// import MyTestimonial from '../panels/MyTestimonial.vue';
 import ContactMe from '../panels/ContactMe.vue';
 import Footer from '../panels/Footer.vue';
 </script>
